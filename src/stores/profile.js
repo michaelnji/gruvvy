96dab38d2434f1d3d1e216c93f1d4b77cfedb8bf
COD3VIL <codebase65@outlook.com>
2024-04-08 16:56:34 +0100

    🐞 fix: quick bugfix
    fixed issue with state not getting data from localStorage

--- src/stores/profile.js
+++ src/stores/profile.js
@@ -1,6 +1,6 @@
 import { setItemValue } from "@/lib/scripts/db";
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useProfile = defineStore('profile', () => {
     const profile = ref({});
